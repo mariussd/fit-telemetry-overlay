97f956d1d580cda14b4e3260a7a0416fd89f3d68
Marius Sørensen Dreyer <marius.s.dreyer@gmail.com>
2025-01-01 19:34:08 +0100

    fix: make parser disregard features without coordinates correctly

--- src/fit-utils/fit-parser.ts
+++ src/fit-utils/fit-parser.ts
@@ -1,72 +1,105 @@
 // @ts-ignore
-import EasyFit from 'easy-fit'
-import {FitData} from '../types'
+import EasyFit from "easy-fit";
+import { FitData } from "../types";
+
+function isNonZeroFloat(value: any) {
+  return (
+    typeof value === "number" && !isNaN(value) && value !== 0 && value % 1 !== 0
+  );
+}
+
+function createCoordinates(
+  startLong: number,
+  startLat: number,
+  endLong: number,
+  endLat: number
+) {
+  const long = [startLong, endLong].filter(value => isNonZeroFloat(value));
+  const lat = [startLat, endLat].filter(value => isNonZeroFloat(value))
+
+  if (!lat.length && !long.length) {
+    return;
+  }
+
+  return [
+    [long[0], lat[0]],
+    [long[long.length - 1], lat[lat.length - 1]],
+  ];
+}
 
 const easyFit = new EasyFit({
   force: true,
-  speedUnit: 'km/h',
-  lengthUnit: 'km',
+  speedUnit: "km/h",
+  lengthUnit: "km",
   elapsedRecordField: true,
-  mode: 'both',
-})
+  mode: "both",
+});
 
 export async function parseFitFile(file: ArrayBuffer): Promise<FitData> {
   return new Promise((resolve, reject) => {
     easyFit.parse(file, (error: any, data: any) => {
       if (error) {
-        console.log(error)
-        reject(error)
+        console.log(error);
+        reject(error);
       }
 
-      resolve(transformToFitData(data))
-    })
-  })
+      resolve(transformToFitData(data));
+    });
+  });
 }
 
 function transformToFitData(data: any) {
   const geo: FitData = {
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: [],
-  }
+  };
 
   if (data && data.records) {
-    let prev_position_long = 0
-    let prev_position_lat = 0
-    let idx_records = 0
-    let element: any = {}
+    let prev_position_long = 0;
+    let prev_position_lat = 0;
+    let idx_records = 0;
+    let element: any = {};
     let pausedTime = 0;
-    for (
-      idx_records = 0;
-      idx_records < data.records.length;
-      idx_records++
-    ) {
-      element = data.records[idx_records]
+    for (idx_records = 0; idx_records < data.records.length; idx_records++) {
+      element = data.records[idx_records];
 
       if (!element.speed) {
         pausedTime += 1;
         continue;
       }
 
-      if (!element.position_long || !element.position_lat) {
+      if (idx_records < 10) {
+        console.log({ long: element.position_long, lat: element.position_lat });
+      }
+
+      const coordinates = createCoordinates(
+        prev_position_long,
+        prev_position_lat,
+        element.position_long,
+        element.position_lat
+      );
+
+      if (!coordinates) {
         continue;
       }
 
       if (idx_records > 0) {
-        let f: any = {}
-        f.type = 'Feature'
-        f.properties = {...element, elapsed_time: element.elapsed_time - pausedTime}
-        f.geometry = {}
-        f.geometry.type = 'LineString'
-        f.geometry.coordinates = [
-          [prev_position_long, prev_position_lat],
-          [element.position_long, element.position_lat],
-        ]
-        geo.features.push(f)
+        let f: any = {};
+        f.type = "Feature";
+        f.properties = {
+          ...element,
+          elapsed_time: element.elapsed_time - pausedTime,
+        };
+        f.geometry = {};
+        f.geometry.type = "LineString";
+        f.geometry.coordinates = coordinates,
+      
+        geo.features.push(f);
       }
-      prev_position_long = element.position_long
-      prev_position_lat = element.position_lat
+      prev_position_long = element.position_long;
+      prev_position_lat = element.position_lat;
     }
   }
 
-  return geo
+  return geo;
 }
