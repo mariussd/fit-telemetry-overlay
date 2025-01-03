590ad65759fbbd8df1434ad2751f04d384ea1cbb
Marius Sørensen Dreyer <marius.s.dreyer@gmail.com>
2025-01-03 21:19:12 +0100

    feat: use correct colors

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ export const WIDTH = 3840
 export const HEIGHT = 2160
 
 export const COLORS = {
-  FG: 'red', // 'rgb(246, 242, 240)',
-  BG: 'blue', // 'rgba(175, 173, 173, 0.64)',
-  DOT: 'rgb(71, 148, 243)'
+  FG: 'rgb(246, 242, 240)',
+  BG: 'rgba(201, 197, 197, 0.64)',
+  DOT: 'rgb(71, 148, 243)',
+  HEART: 'rgba(239, 79, 79, 0.95)',
 }
