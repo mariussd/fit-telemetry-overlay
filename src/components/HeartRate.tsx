75dc407e708e5da0f91950e22481292cfe0bfc31
Marius Sørensen Dreyer <marius.s.dreyer@gmail.com>
2025-01-03 21:19:32 +0100

    feat: style the heart rate component properly

--- src/components/HeartRate.tsx
+++ src/components/HeartRate.tsx
@@ -4,39 +4,46 @@ type Props = {
   heartRate: number;
 };
 
+let validatedHeartRate = 0;
+
 export const HeartRate = ({ heartRate }: Props) => {
   console.log({ heartRate });
 
-  const width = 800;
-  const height = 400;
+  const width = 1200;
+  const height = 600;
+
+  if (!!heartRate) {
+    validatedHeartRate = heartRate;
+  }
+
   return (
     <svg
-      viewBox={`0 0 ${width / 2} ${height / 2}`}
+      viewBox={`0 -60 ${width / 2} ${height / 2}`}
       xmlns="http://www.w3.org/2000/svg"
       fill={COLORS.BG}
     >
       <path
-      d="
+        d="
         M 100 200 
         V 50 
         H 200 
         A 50,50 90 0,1 200,150 
         A 50,50 90 0,1 100,150 
         Z"
-      fill="red"
-      transform="rotate(225,150,121)"
-    />
+        fill={COLORS.HEART}
+        transform="rotate(225,150,121)"
+      />
       <text
-      x="260"
-      y="100"
-      font-size="16"
-      font-family="Arial, sans-serif"
-      fill="#333"
-      text-anchor="start"
-      dominant-baseline="middle"
-    >
-      {heartRate}
-    </text>
+        x="260"
+        y="140"
+        fontSize="140"
+        fontFamily="Helvetica"
+        fill={COLORS.FG}
+        textAnchor="start"
+        dominantBaseline="middle"
+      >
+        {validatedHeartRate}
+      </text>
       <div style={{ fontSize: 100 }}>{heartRate}</div>
     </svg>
   );
